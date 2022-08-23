9e49cf1c637b77108cd6964fb5a05b3430282526
MarckVinny <mvrpj@msn.com>
2022-08-23 00:26:00 -0300

    Exercício desafio 02

---
+++ mundo01/exec001.py
@@ -0,0 +1,6 @@
+print ('====== Desafio 02 =====')
+print ('Qual sua data de nascimento?')
+dia = input ('Qual é o dia?  ')
+mes = input ('Qual é o mês?  ')
+ano =input ('Qual é o ano?  ')
+print ('Você nasceu no dia', dia, 'de', mes, 'de', ano,', correto?')
