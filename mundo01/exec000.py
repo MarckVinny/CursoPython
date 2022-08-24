10cd5210b6bc3e72882ddb5d4ed1f274d0319f6e
MarckVinny <mvrpj@msn.com>
2022-08-24 20:33:19 -0300

    Saída Formatada     {} usando as chaves dentro da string     .format(variável) fora da string
        Esta função está presente a partir do Python 3

--- mundo01/exec000.py
+++ mundo01/exec000.py
@@ -1,3 +1,9 @@
 print ('====== Desafio 01 =====')
 nome = input ('Qual é o seu nome?')
-print ('Olá', nome,', prazer em te conhecer!')
+print ('Olá {}, prazer em te conhecer!'.format(nome))
+"""Saída Formatada
+    {} usando as chaves dentro da string
+    .format(variável) fora da string
+    
+    Esta função está presente a partir do Python 3
+"""
