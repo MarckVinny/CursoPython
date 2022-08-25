9efe2eb1ca3479eeab1cb21b792b1fcd3f54960c
MarckVinny <mvrpj@msn.com>
2022-08-25 01:39:08 -0300

    Tipos Primitivos e correção exercício da Aula 04

--- mundo01/exercicios/aula_4a.py
+++ mundo01/exercicios/aula_4a.py
@@ -18,4 +18,7 @@ n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro: '))
 s = n1 + n2
 
-print('A soma vale: {}'.format(s))
+# Forma Obsoleta
+# print('A soma entre ', n1, 'e', n2, 'vale: ', s)
+
+print('A soma entre {} e {} vale: {}'.format(n1, n2, s))
